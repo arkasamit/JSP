26c12c037e0cf743783f88228d924473c457881b
arkasarkeramit <arkaamitdev@gmail.com>
2025-05-25 23:52:23 +0600

    complete task 2

--- Conditional_task.js
+++ Conditional_task.js
@@ -10,4 +10,36 @@ else{
     console.log("Coke Price 30tk only");
     
 }
-hello Git
+
+let studentMarks = 55;
+
+console.log("My Score is -", studentMarks);
+
+if (studentMarks <= 100 && studentMarks >= 90) {
+    console.log("hurrah I Got A");
+    
+}
+
+else if (studentMarks <= 89 && studentMarks >= 80) {
+     console.log("I Got B");
+}
+
+else if (studentMarks <= 79 && studentMarks >= 70) {
+     console.log("I Got C");
+}
+
+else if (studentMarks <= 69 && studentMarks >= 60) {
+     console.log("I Got D");
+}
+
+else if (studentMarks <= 59 && studentMarks >= 33) {
+     console.log("I Got D");
+}
+
+else {
+
+console.log("again Try");
+
+}
+
+
