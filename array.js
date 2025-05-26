1b8bf4748a4407e0cbec787fe5650712dedb7710
arkasarkeramit <arkaamitdev@gmail.com>
2025-05-26 22:43:10 +0600

    task 3 complete of array

---
+++ array.js
@@ -0,0 +1,44 @@
+// Task 1
+const fruits = ["mango","jackfruits","Watermelon","Lichi","apple"];
+console.log(fruits[3]);
+fruits[3] = "jambura";
+
+console.log(fruits);
+
+// Task 2
+
+const tourist = ["Cox-Bazar","Kashmir","Ayodhaya"];
+
+tourist.push("Saint-martin"); //added one tourist place 
+tourist.push("Bogura","Rangpur");//added another two tourist place
+tourist.pop("Rangpur");// remove lasrt tourist place 
+
+console.log(tourist);//final result
+console.log(tourist.length);//array length output
+
+// Task 3
+
+const books = ["JS","C","Python","C++","Digital Maketing"];
+
+if (books.includes("JS")) {
+
+    console.log("I will read");
+    
+
+} else {
+    console.log("Books not found");
+    
+}
+
+// Task 4
+
+const age = [43, 36, 73];
+const price  = 34;
+const isStudent = true;
+const names = "Rahim";
+
+console.log(Array.isArray(age)? "Age is an array" : "Age not an array");
+console.log(Array.isArray(price)?"Price is an array" : "Price is a product price");
+console.log(Array.isArray(isStudent)? "isStudent is an array" : "isStudent is a boolean") 
+console.log(Array.isArray(names)? "Name is an array" : "names is a variable");
+
