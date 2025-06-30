7121034c2313a84d8e12e19fbd743c9a5f00837c
arkasarkeramit <arkaamitdev@gmail.com>
2025-06-30 22:19:35 +0600

    Complete third task ultiplication table for number 9

---
+++ loop_3.js
@@ -0,0 +1,10 @@
+// Task 4 - Generate a multiplication table for number 9
+
+let multiply = 0;
+
+for(i = 1; i<=10; i++){
+
+    multiply = 9 * i;
+    console.log("9 *", i, "=", multiply);
+    
+}
