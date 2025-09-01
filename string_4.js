6a7059ba46cda5e91c47657f223c95ce594a7b06
arkasarkeramit <arkaamitdev@gmail.com>
2025-09-01 22:30:33 +0600

    solve capital issues

--- string_4.js
+++ string_4.js
@@ -4,3 +4,20 @@ let statement = "my ex is always is X";
 let result = statement.replaceAll("x", "y").replaceAll("X", "Y");
 
 console.log(result);
+
+
+
+// Task 5 Capitalize Every first Letter of each word in a String
+
+let myName = "arka sarker amit";
+let convertArray = myName.split(' ');
+let capitalFirstWord = [];
+
+for (let i = 0; i < convertArray.length; i++) {
+
+        let word = convertArray[i];
+        capitalFirstWord[i] = word.charAt(0).toUpperCase("")+ word.slice(1);
+
+}
+
+console.log(capitalFirstWord.join(' '));
