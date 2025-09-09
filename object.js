7747a5cba37145c41ad4a9b809cf7368f596c860
arkasarkeramit <arkaamitdev@gmail.com>
2025-09-09 22:18:50 +0600

    Today I have practice previous task

--- object.js
+++ object.js
@@ -17,5 +17,27 @@ for (let i = 0; i < keyArray.length; i++) {
     
 }
 
+// 2nd type
+
+let myObj = {
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+let convertArray = Object.keys(myObj);
+let  result = Object.values(myObj);
+
+for (let i = 0; i < convertArray.length; i++) {
+     
+  
+    console.log(`${convertArray[i]} | ${typeof(result[i])}`);
+
+    
+}
+
+
+
 // The property name (key) isStudent is a string because all keys in a JavaScript object are strings.
 // It looks like the key isStudent isn't a string. But behind the scenes, JavaScript automatically converts it to a string.
