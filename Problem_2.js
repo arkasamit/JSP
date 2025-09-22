05575cde841149c9e6ba5b99e371416da2e62a46
arkasarkeramit <arkaamitdev@gmail.com>
2025-09-22 23:06:03 +0600

    improve code' clear

--- Problem_2.js
+++ Problem_2.js
@@ -2,18 +2,21 @@ function leapYear(year) {
     
 if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
     
-    // return (`${year} year is leap year`); 
-    return true;
+    return "year is leap year"; 
+    // return true;
 }
 else{
  
-    // return (`${year} year is normal year`);  
-    return false;
+    return "year is normal year";  
+    // return false;
 }
 }
 
 const inputYear = leapYear(2024);
-console.log(inputYear);
+const inputYear_2 = leapYear(2025);
+const inputYear_3 = leapYear(1992);
+const inputYear_4= leapYear(1991);
+console.log(`2024 = ${inputYear}, 2025 = ${inputYear_2}, 1992 = ${inputYear_3}, 1991 = ${inputYear_4}`);
 
 
 // const leapYear = year => 
