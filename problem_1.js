03a5127f2c41a5dfbc3751fffff9d53078583c14
arkasarkeramit <arkaamitdev@gmail.com>
2025-09-22 22:40:12 +0600

    complete inch to feet and mile to Kilometer conversion

--- problem_1.js
+++ problem_1.js
@@ -1,3 +1,19 @@
-function name(params) {
+function conversion(inch , mile) {
+
+    // convert inch to feet
+
+    const convertFeet = Math.floor(inch/12);    //I can also use convertInterger = parseInt(convertFeet);
+    const fractionValue = inch % 12;
+    const totalFeet_inch = `${convertFeet} feet  ${fractionValue} inch`;
+    
+    // convert to km
+
+    const convertKm = mile * 1.60934;
+    const totalKm = `${mile} = ${convertKm} km`;
+
+    return {totalFeet_inch, totalKm}
     
 }
+
+const result = conversion(68, 5);
+console.log(result);
