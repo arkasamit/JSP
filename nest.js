caa00ff7971b78d53ccdbe2ceb1d495c5729ef0a
arkasarkeramit <arkaamitdev@gmail.com>
2025-09-22 13:11:11 +0600

    complete two problem of function

--- nest.js
+++ nest.js
@@ -1,28 +1,28 @@
-let myScore = 85; 
-let frndScore = 80;
+// let myScore = 85; 
+// let frndScore = 80;
 
-if (myScore >= 80) {
-    console.log("You're inside your friend's score.");
+// if (myScore >= 80) {
+//     console.log("You're inside your friend's score.");
    
-    if (frndScore >= 80 ) {  
-        console.log('Go for lunch');
-    }
-   else if (frndScore < 80 && frndScore >= 60 ) {
-        console.log('Good Luck for next time');
-   }
+//     if (frndScore >= 80 ) {  
+//         console.log('Go for lunch');
+//     }
+//    else if (frndScore < 80 && frndScore >= 60 ) {
+//         console.log('Good Luck for next time');
+//    }
 
-    else if (frndScore < 60 && frndScore >= 40 ){
+//     else if (frndScore < 60 && frndScore >= 40 ){
           
-        console.log('keep your friend message unseen');
+//         console.log('keep your friend message unseen');
 
-    }
-    else if ( frndScore < 40) {
-      console.log('Block you');
+//     }
+//     else if ( frndScore < 40) {
+//       console.log('Block you');
       
-    }   
-} 
-else {
-    console.log("Go home, sleep 😴, and act sad 😢.");
+//     }   
+// } 
+// else {
+//     console.log("Go home, sleep 😴, and act sad 😢.");
    
-}
+// }
 
