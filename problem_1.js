9068767c30d3c8f3231d483be57aebd9fa3c26a9
arkasarkeramit <arkaamitdev@gmail.com>
2025-09-22 22:28:49 +0600

    added file

--- problem_1.js
+++ problem_1.js
@@ -1,21 +1,3 @@
-// Inch to Feet, miles to kilometre
-
-function convert(i, m) {
+function name(params) {
     
-    // convert inch
-    const convert_feet = i / 12;
-    const modifyInterger = parseInt(convert_feet); // I can also use here math.floor(i/12);
-    const convert_inch = i % 12;
-    const totalFeet_inch =  `${modifyInterger}feet ${convert_inch} inch`;
-
-    // convert to mile
-    let convert_mile = m * 1.609;
-
-    // value return
-    return {totalFeet_inch , convert_mile}
-      
 }
-
-let convert_result = convert(75, 2);
-console.log(convert_result);
-
