760e4150dbd711f0135886eede3c50830547bc19
arkasarkeramit <arkaamitdev@gmail.com>
2025-09-26 13:00:29 +0600

    complete the hole task

---
+++ day/Task_2.js
@@ -0,0 +1,37 @@
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+// sample-input: numbers = [5,6,11,12,98, 5]
+
+// find: 5
+
+// output: 2
+
+
+// sample-input:
+
+// numbers = [5,6,11,12,98, 5]
+
+// find: 25
+
+// output: 0
+
+function arrayInput(arrayInfo, find) {
+   let count = 0;
+
+  for (let i = 0; i < arrayInfo.length; i++) {
+  
+    if (arrayInfo[i] === find) {
+        
+        count++;
+    } 
+  }
+ return count;
+   
+
+}
+
+const numbers = [5,6,11,12,98, 5];
+const result = arrayInput(numbers, 5);
+console.log(result);
+const result_2 = arrayInput(numbers, 25);
+console.log(result_2);
