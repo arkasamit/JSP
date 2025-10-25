a34d55318b1307816daf067c7f0c65dbdbcb7351
arkasarkeramit <arkaamitdev@gmail.com>
2025-10-26 01:35:57 +0600

    complete marry program

---
+++ assignment/marry.js
@@ -0,0 +1,36 @@
+function  marriage(groom, bride) {
+
+    if (typeof groom !== "object" || typeof bride !== "object") {
+
+        return "Invalid"
+    }
+
+    let diffence = Math.abs(groom.age - bride.age);
+
+    if(groom.gender !== bride.gender && diffence <= 7 && groom.age > bride.age){
+
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+const boy = {name: "Arka", gender: "male", age: 30};
+const girl = {name: "Pritha", gender: "female", age: 32};
+
+const boy1 = {name: "Arka", gender: "male", age: 30};
+const girl1 = {name: "Pritha", gender: "female", age: 20};
+
+const boy2 = {name: "Arka", gender: "male", age: 30};
+const girl2 = {name: "Pritha", gender: "female", age: 26};
+
+const boy3 = {name: "Arka", gender: "male", age: 30};
+const girl3 = "My name is Prithe, I am female and my age 20";
+
+console.log(marriage(boy, girl));
+console.log(marriage(boy1, girl1));
+console.log(marriage(boy2, girl2));
+console.log(marriage(boy3, girl3));
+
