44e8bf05519f21472980bfa3409d4fe0723ea200
arkasarkeramit <arkaamitdev@gmail.com>
2026-02-19 21:13:40 +0600

    complete array loop task

--- review.js
+++ review.js
@@ -286,3 +286,51 @@ const statements = 'I am a hard working person';
 const newStatements = statement.split(' ').reverse().join(' ');
 console.log(newStatements);
 
+// Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+
+// Input: [1, 2, 3]
+
+const original = [1, 2, 3];
+const copyArray = original.slice(0);
+copyArray[0] = 99;
+console.log(copyArray);
+console.log(original);
+
+  // Given an array of student objects, print each student’s name and marks.
+  // [
+  //   { name: "John", marks: 85 },
+  //   { name: "Alice", marks: 90 }
+  // ]
+
+  const studentArray = [
+    { name: "John", marks: 85 },
+    { name: "Alice", marks: 90 }
+  ]
+
+  for (let i = 0; i < studentArray.length; i++) {
+  
+    console.log(`${studentArray[i].name} scored ${studentArray[i].marks}`);
+    
+  }
+
+  //   Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+  // input:
+
+  // [
+  //   [1, 2],
+  //   [3, 4],
+  //   [5, 6]
+  // ]
+
+  const twoDarray =  [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+  ]
+  twoDarray[1][0] = 99;            
+  console.log(twoDarray);
+
+// For uderstand how declace in spilce method is 
+// Replace first element of second row
+// twoDarray[1].splice(0, 1, 99);
